b21046d847848d102cc847b3ebe2bfbb15071ad3
Radoslaw Nowak <radoslawnowak@gmail.com>
2025-03-06 22:50:21 +0100

    feat: add new screen 'special offerts

--- navigation/MainMenu.tsx
+++ navigation/MainMenu.tsx
@@ -1,22 +1,10 @@
 import * as React from 'react';
-import { View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { useNavigation } from '@react-navigation/native';
-import { Button } from '@react-navigation/elements';
 
 //components
 import HomeScreen from '@/screens/HomeScreen/HomeScreen';
 import ProductsList from '@/screens/ProductsList/ProductsList';
-
-function NotificationsScreen() {
-  const navigation = useNavigation();
-
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => navigation.goBack()}>Go back home</Button>
-    </View>
-  );
-}
+import SpecialOffertsList from '@/screens/specialOffertsList/SpecialOffertsList';
 
 const Drawer = createDrawerNavigator();
 
@@ -25,6 +13,7 @@ export default function MainMenu() {
     <Drawer.Navigator initialRouteName='Home'>
       <Drawer.Screen name='Home' component={HomeScreen} />
       <Drawer.Screen name='Products list' component={ProductsList} />
+      <Drawer.Screen name='Special offerts' component={SpecialOffertsList} />
     </Drawer.Navigator>
   );
 }
