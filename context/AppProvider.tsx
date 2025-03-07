506df9006516fbc848e07229c8ebd2347e85a86a
Radoslaw Nowak <radoslawnowak@gmail.com>
2025-03-07 12:36:00 +0100

    feat: add state to asyncStorage

--- context/AppProvider.tsx
+++ context/AppProvider.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Product } from '@/types/Product';
 import { SpecialOffertProduct } from '@/types/SpecialOffertProduct';
 import { AppContextType } from '@/types/AppContextType';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const AppContext = React.createContext<AppContextType>({
   products: [],
@@ -19,6 +20,35 @@ const AppProvider = ({ children }: { children: React.ReactNode }) => {
   const [specialOffertProducts, setSpecialOffertsProducts] = React.useState<
     SpecialOffertProduct[]
   >([]);
+
+  const saveToStorage = async (key: string, value: any) => {
+    try {
+      await AsyncStorage.setItem(key, JSON.stringify(value));
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const loadFromStorage = async (key: string, setter: (data: any) => void) => {
+    try {
+      const storedData = await AsyncStorage.getItem(key);
+      if (storedData) {
+        setter(JSON.parse(storedData));
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  React.useEffect(() => {
+    loadFromStorage('products', setProducts);
+    loadFromStorage('specialOffertProducts', setSpecialOffertsProducts);
+  }, [setProducts, setSpecialOffertsProducts]);
+  React.useEffect(() => {
+    saveToStorage('products', products);
+  }, [products]);
+  React.useEffect(() => {
+    saveToStorage('specialOffertProducts', specialOffertProducts);
+  }, [specialOffertProducts]);
 
   const addProductToProducts = (newProduct: Product) => {
     setProducts((prevProducts) => [...prevProducts, newProduct]);
