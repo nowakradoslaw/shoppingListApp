986f1ede0c92803ecf096d2ed8848df5095b0a75
Radoslaw Nowak <radoslawnowak@gmail.com>
2025-03-09 21:37:21 +0100

    feat: add transper product from it to special offerts

--- screens/ProductsList/ProductsList.tsx
+++ screens/ProductsList/ProductsList.tsx
@@ -5,19 +5,46 @@ import { FlatList, Pressable } from 'react-native-gesture-handler';
 import { styles } from '@/screens/ProductsList/ProductsList.style';
 import ProductsListModal from '@/components/ProductsListModal/ProductsListModal';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import SpecialOffertsModal from '@/components/specialOffertsModal/SpecialOffertsModal';
 const ProductsList = () => {
   const { products, deleteProductFromProducts } = React.useContext(AppContext);
   const [visibleModal, setVisibleModal] = React.useState<boolean>(false);
+  const [productName, setProductName] = React.useState<string>('');
+  const [specialOffertModalVisible, setSpecialOffertModalVisible] =
+    React.useState<boolean>(false);
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Product List</Text>
+      <View style={styles.headingWrapper}>
+        <Text style={styles.headingItem}>#</Text>
+        <Text style={styles.headingItem}>Product name</Text>
+        <Text style={styles.headingItem}>Transfer</Text>
+        <Text style={styles.headingItem}>Delete</Text>
+      </View>
       <FlatList
         data={products}
         renderItem={({ item, index }) => (
           <View style={styles.flatListItemWrapper}>
             <Text style={styles.flatListItemText}>{index + 1}. </Text>
             <Text style={styles.flatListItemText}>{item.name}</Text>
-            <Pressable onPress={() => deleteProductFromProducts(item.id)}>
+            <Pressable
+              style={styles.buttonItem}
+              onPress={() => {
+                setSpecialOffertModalVisible(true),
+                  setProductName(item.name),
+                  deleteProductFromProducts(item.id);
+              }}
+            >
+              <Ionicons
+                name='swap-horizontal-outline'
+                size={24}
+                color='black'
+              />
+            </Pressable>
+            <Pressable
+              style={styles.buttonItem}
+              onPress={() => deleteProductFromProducts(item.id)}
+            >
               <Ionicons name='trash-outline' size={24} color='red' />
             </Pressable>
           </View>
@@ -29,6 +56,12 @@ const ProductsList = () => {
       <ProductsListModal
         visibleModal={visibleModal}
         setVisibleModal={setVisibleModal}
+      />
+      <SpecialOffertsModal
+        modalVisible={specialOffertModalVisible}
+        setModalVisible={setSpecialOffertModalVisible}
+        product={productName}
+        setProduct={setProductName}
       />
     </View>
   );
